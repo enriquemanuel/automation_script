ca40775eb5d437387c9d08b8e7444dba6c362866
Enrique Valenzuela <em@enriquemanuel.me>
2016-06-06 09:50:05 -0400

    new functions and trying to connect

--- automator.sh
+++ automator.sh
@@ -157,6 +157,7 @@ declare -a vACTIONS=('Data Mining' 'List Patches' 'Execute Audits Listing (APP)'
 # Step 4: Confirm Sub action
 
 # STEP 1: Display the list of possible actions
+echo ""
 echo "${normal}What ${red}Action${normal} would you like to execute"
 vCOUNTER=0
 for action in "${vACTIONS[@]}"
@@ -257,6 +258,19 @@ if [[ "$vSELECTEDACTION" == "Data Mining" ]]; then
   else
   	vDATE="."$vCURDATE".txt.gz"
   fi
+
+  # connect to SSH
+  #vCOUNTER=0
+  #for h in "${vAPPSIP[@]}";
+  #do
+  #	echo "Connecting to ${vAPPS[$vCOUNTER]}"
+  #	ssh  -o StrictHostKeyChecking=no $vUSERNM@$h zgrep --color=auto -H $vUSERPK1 /usr/local/blackboard/logs/tomcat/bb-access-log$vDATE | awk '{print $1, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18}'
+  #	ssh  -o StrictHostKeyChecking=no $vUSERNM@$h grep --color=auto -H $vUSERPK1 /usr/local/blackboard/asp/*/tomcat/bb-access-log$vDATE | awk '{print $1, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18}'
+  #	echo "Disconnecting from ${vAPPS[$vCOUNTER]}"
+  #	echo "---------------------"
+  #	echo ""
+  #	vCOUNTER=$[$vCOUNTER +1]
+  #done
 
 
 elif [[ "$vSELECTEDACTION" == "List Patches" ]]; then
