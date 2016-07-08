a0a468caf9067a93d9fa49d50680cd92d440c821
Enrique Valenzuela <em@enriquemanuel.me>
2016-07-08 10:09:56 -0400

    added security to search for clients and environments, as well as date validation check

--- automator.sh
+++ automator.sh
@@ -8,7 +8,8 @@
 
 #==============================================================================
 
-## 2016-05-20 - Beta release date. Written by Enrique Valenzuela
+## 2016-05-20 - Beta release date.
+## Written by Enrique Valenzuela
 
 ## Before we get started, make sure this is being run from a writeable location.
 cwd=`pwd`
@@ -45,37 +46,59 @@ read -p "Please provide your ${red}MH username:${normal}${bold} " vUSERNM
 # Download file using SCP to current directory
 echo "${normal}We will download the Client Database file into a temporal location..."
 scp -pq $vUSERNM@10.6.11.11:/mnt/asp/utils/bin/include/ops_webtech_data.txt ./
-echo "File downloaded."
-
-# Ask for Client Name
-read -p "${normal}What ${red}client${normal}  do you want to work on: ${bold}" vCLIENTNAME
-# Ask for Environment type (Production or Staging or Test)
-read -p "${normal}What ${red}environment${normal} do you want to work on (Production, Staging, Test...): ${bold}" vENVIRONMENT
-echo "${normal}"
-
-# Get unique URLS and ask client which one they want to work on
-vOPTIONS=($(grep --color=auto -i "$vCLIENTNAME" $vFILENAME | grep --color=auto -i $vENVIRONMENT | awk 'BEGIN { FS = "\t" } ; {print $14}' | sort | uniq))
-
-# Send to the user the list of URLS that we found and make them select one
-echo "${green}We found this options: ${normal}"
-vCOUNTER=0
-for i in "${vOPTIONS[@]}"
-do
-  echo "$vCOUNTER) $i"
-  vCOUNTER=$[$vCOUNTER +1]
-done
+echo
+echo ">>> File downloaded ..."
 echo
 
+# Security Loop
+# Get Input of Client and Environment
+# If nothing is found then ask again.
+until ((0));
+do
+  # Ask for Client Name
+  read -p "${normal}What ${red}client${normal}  do you want to work on: ${bold}" vCLIENTNAME
+  # Ask for Environment type (Production or Staging or Test)
+  read -p "${normal}What ${red}environment${normal} do you want to work on (Production, Staging, Test...): ${bold}" vENVIRONMENT
+  echo "${normal}"
+
+  vOPTIONS=($(grep --color=auto -i "$vCLIENTNAME" $vFILENAME | grep --color=auto -i $vENVIRONMENT | awk 'BEGIN { FS = "\t" } ; {print $14}' | sort | uniq))
+
+  if [ ${#vOPTIONS[@]} -eq 0 ]; then
+    echo "${normal} -------------------------------------------------------------------------------------"
+    echo "${bold}${red}ERROR:${normal} We could not found any information with your parameters, please try again."
+    echo "-------------------------------------------------------------------------------------"
+    echo
+  else
+    echo "${green}We found this options: ${normal}"
+    vCOUNTER=0
+    for i in "${vOPTIONS[@]}"
+    do
+      echo "$vCOUNTER) $i"
+      vCOUNTER=$[$vCOUNTER +1]
+    done
+    echo
+    break
+  fi
+done
+
+
 # Ask to select one of the options above
-read -p "Input the above ${red}id number${normal} you want to work on: ${bold}" vARRAYID
-# Set the Working url
-vWORKINGURL=${vOPTIONS[$vARRAYID]}
+until ((0));
+do
+  # Ask to select one of the options above
+  read -p "Input the above ${red}id number${normal} you want to work on: ${bold}" vARRAYID
+  # Set the Working url
+  vWORKINGURL=${vOPTIONS[$vARRAYID]}
 
-
-if [ "$vWORKINGURL" == "" ]; then
-	echo "ERROR: Wrong Input, exiting"
-	exit 1
-fi
+  if  [[ "$vWORKINGURL" == "" ]]; then
+    echo "${normal} -------------------------------------------------------------------------------------"
+    echo "${bold}${red}ERROR:${normal} Invalid option, please try again."
+    echo "-------------------------------------------------------------------------------------"
+    echo
+  else
+    break
+  fi
+done
 
 # Now lets find the App Servers to work
 vTEMPAPPS=($(grep --color=auto -i "$vCLIENTNAME" $vFILENAME | grep --color=auto -i $vENVIRONMENT | grep --color=auto -i $vWORKINGURL | awk 'BEGIN { FS="\t"}; {print $3}' | sed  's/_/-/g'))
@@ -96,8 +119,6 @@ for eachapp in "${vAPPS[@]}"; do
 done
 
 
-
-
 # deleting the file so we are always up to date
 rm -rf $vFILENAME
 
@@ -112,12 +133,35 @@ echo
 echo "${bold}NOTE: ${normal}If the above is not correct, please CTRL+C to exit the app and restart it."
 echo
 
+
 # Ask for a specific date in regular expresion to search for
-vCURDATE=`date +%Y-%m-%d`
-read -p "Input the ${red}Start Date${normal} you want to search (YYYY-MM-DD) (e.g: lower than end date): ${bold}" vSTARTDATE
-echo "${normal}"
-read -p "Input the ${red}End Date${normal} you want to search (YYYY-MM-DD) (e.g: higher than start date): ${bold}" vENDDATE
-echo "${normal}"
+until ((0)); do
+  read -p "Input the ${red}Start Date${normal} you want to search (YYYY-MM-DD) (e.g: lower than end date): ${bold}" vSTARTDATE
+  echo "${normal}"
+  if [[ $vSTARTDATE =~ ^[0-9]{4}-(0[0-9]|1[0-2])-([0-2][0-9]|3[0-1])$ ]]; then
+    break
+  else
+    echo "${normal} -------------------------------------------------------------------------------------"
+    echo "${bold}${red}ERROR:${normal} Invalid Start date, please try again."
+    echo "-------------------------------------------------------------------------------------"
+    echo
+  fi
+done
+
+until ((0)); do
+  read -p "Input the ${red}End Date${normal} you want to search (YYYY-MM-DD) (e.g: higher than start date): ${bold}" vENDDATE
+  echo "${normal}"
+  if [[ $vENDDATE =~ ^[0-9]{4}-(0[0-9]|1[0-2])-([0-2][0-9]|3[0-1])$ ]]; then
+    break
+  else
+    echo "${normal} -------------------------------------------------------------------------------------"
+    echo "${bold}${red}ERROR:${normal} Invalid End date, please try again."
+    echo "-------------------------------------------------------------------------------------"
+    echo
+  fi
+done
+
+
 
 # Create Date Ranges
 if date -v 1d > /dev/null 2>&1; then
@@ -140,6 +184,8 @@ else
   done
 fi
 
+# new comment
+
 # Ask what user pk1 to search for
 read -p "Input the ${red}Information String${normal} you want to search (wrap in double quotes if using special characters): ${bold}" vSTRINGSEARCH
 echo "${normal}"
@@ -149,8 +195,8 @@ vCOUNTER=0
 for h in "${vAPPSIP[@]}"; do
   echo "Connmecting to ${vAPPS[$vCOUNTER]}"
   for day in "${datearrange[@]}"; do
-    ssh -o StrictHostKeyChecking=no $vUSERNM@$h grep --color=auto -H $vSTRINGSEARCH /usr/local/blackboard/logs/tomcat/bb-access-log.$day.txt | awk '{print $1, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18}'
-    ssh -o StrictHostKeyChecking=no $vUSERNM@$h zgrep --color=auto $vSTRINGSEARCH /usr/local/blackboard/asp/${vAPPS[$vCOUNTER]}/tomcat/bb-access-log.$day.txt.gz | awk '{print $1, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18}'
+    ssh -o StrictHostKeyChecking=no $vUSERNM@$h grep --color=auto -H $vSTRINGSEARCH /usr/local/blackboard/logs/tomcat/bb-access-log.$day.txt | awk '{print $1, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18}' >> ~/Desktop/tmp.log
+    ssh -o StrictHostKeyChecking=no $vUSERNM@$h zgrep --color=auto $vSTRINGSEARCH /usr/local/blackboard/asp/${vAPPS[$vCOUNTER]}/tomcat/bb-access-log.$day.txt.gz | awk '{print $1, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18}' >> ~/Desktop/tmp.log
   done
   echo "Disconnecting from ${vAPPS[$vCOUNTER]}"
   echo ""
